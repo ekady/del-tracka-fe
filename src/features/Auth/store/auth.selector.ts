309af8b7218cc92934bf51871d22aeb7190a5900
Dyah Eka Lestari <98830227+ekady@users.noreply.github.com>
2023-09-21 05:57:10 +0700

    Delete src/features/Auth directory

--- src/features/Auth/store/auth.selector.ts
+++
@@ -1,4 +0,0 @@
-import { RootState } from '@/common/store';
-
-export const selectCredential = (state: RootState) => state.auth.data.ICredential;
-export const selectCredentialToken = (state: RootState) => state.auth.data.ICredential.accessToken;
