11323e10ccce1744dbefc6268140db263c866bd4
Dyah Eka Lestari <98830227+ekady@users.noreply.github.com>
2023-09-21 05:58:11 +0700

    Delete src/features/Projects directory

--- src/features/Projects/components/ProjectSetting/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as ProjectInformation } from './ProjectInformation';
-export { default as ProjectFormNewMember } from './ProjectFormNewMember';
-export { default as ProjectMemberList } from './ProjectMemberList';
-export { default as ProjectMembers } from './ProjectMembers';
-export { default as ProjectOtherSetting } from './ProjectOtherSetting';
--- src/features/Projects/constant/projectMenu.ts
+++
@@ -1,7 +0,0 @@
-export enum ProjectMenu {
-  PROJECT = 'PROJECT',
-  MEMBER = 'MEMBER',
-  STAGE = 'STAGE',
-  TASK = 'TASK',
-  COMMENT = 'COMMENT',
-}
